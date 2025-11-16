d335295acedd0212b9cabac67585d83279267031
Kyzach22 <luanhhao491@gmail.com>
2025-11-16 12:09:25 +0700

    Kết nối database

---
+++ DoAnThucHanhLapTrinhWeb/ket_noi_database/model/connect.php
@@ -0,0 +1,15 @@
+<?php
+function connectdb(){
+   $servername = "localhost";
+   $username = "root";
+   $password = "";
+   try {
+      $conn = new PDO("mysql:host=$servername;dbname=shopthoitrang", $username, $password);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   } catch (PDOException $th) {
+      echo "Kết nối thất bại: ".$th->getMessage();
+      return null;
+   }
+   return $conn;
+}
+?>
